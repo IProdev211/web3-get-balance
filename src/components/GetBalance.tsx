950c3b9d7ed12d3f84fdc05d7ba3d89e331dba46
prodev <passionguy211@gmail.com>
2022-02-06 15:21:57 -0500

    Added validation for wallet address

--- src/components/GetBalance.tsx
+++ src/components/GetBalance.tsx
@@ -7,9 +7,16 @@ import { tokenList } from '../constants';
 
 export default function GetBalance() {
     const [address, setAddress] = useState<string>('');
+    const [error, setError] = useState<boolean>(false);
 
     const handleChangeAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAddress(e.target.value);
+        setError(false);
+        const addr = e.target.value;
+        if (/0x[a-fA-F0-9]{40}/.test(addr)) {
+            setAddress(addr);
+        } else {
+            setError(true);
+        }
     };
 
     return (
@@ -27,6 +34,11 @@ export default function GetBalance() {
             </Text>
             <Box pt="2">
                 <Input variant="outline" placeholder="Enter the address" color="white" onChange={handleChangeAddress} />
+                {error && (
+                    <Text color="red.300" fontSize="xs" mt="1" ml="2">
+                        Invalid Address
+                    </Text>
+                )}
 
                 <Box p="5" pb="2" mt="4" border="2px solid rgb(45, 55, 72)" borderRadius="24">
                     <Text color="white" fontSize="sm" mb={3}>
